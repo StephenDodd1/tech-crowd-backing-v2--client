6cd801ba0aa12a5725b40e24dc59dce2a616ddfd
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-10 11:36:29 -0500

    added content to landing page

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -2,15 +2,29 @@ import React from 'react';
 
 export default function Landing() {
    return(
-      <div>
-         <p>Content</p>
-         <p>Content</p>
-         <p>Content</p>
-         <p>Content</p>
-         <p>Content</p>
-         <p>Content</p>
-         <p>Content</p>
-         
-      </div>
+      <main>
+         <div>
+            <h3>Introduction</h3>
+            <p>Welcome to TechCrowdBacking! A site for techies to discuss 
+               investments and investors to discuss tech! </p>
+         </div>
+         <div>
+            <h3>Information</h3>
+            <p>We hope you're excited for what's to come, because with tech 
+               comes myriad possibilities. The aim of TechCrowdBacking is to
+               provide you with information on investments and technologies 
+               that will change the world. As Ghandi would have said "You 
+               must be the change you wish to see in the world". So back the 
+               tech you believe in.</p>
+         </div>
+         <div>
+            <h3>Instructions</h3>
+            <ul>
+               <li>Create an account so that you can post and comment.</li><br /> 
+               <li>Write a post on a technology or company that interests you.</li><br /> 
+               <li>Invest in the future you want to see.</li>
+            </ul>
+         </div>
+      </main>
    )
 }
