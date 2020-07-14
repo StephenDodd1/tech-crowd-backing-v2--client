f3e7564c2e5254c4a06fd3794e9dd1a0a617044d
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-14 09:51:35 -0500

    added get request for client to populated posts

--- src/Comment/Comment.js
+++ src/Comment/Comment.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Comment extends Component {
+
+   componentDidMount() {
+      fetch('http://localhost:8000/content/:postId/comment', {
+         method: 'POST',
+         mode: 'cors',
+         credentials: 'same-origin',
+         headers: {
+            'Content-type': 'application/json'
+         }
+      })
+      .then(res => res.json())
+      .then(data => {
+      })
+   }
    render() {
+      
       return(
          <div>
-            <p>Post Title</p>
             <button>DELETE</button>
             <form>
                <textarea placeholder='Enter your comment here. Remember to be respectful.' />
--- src/Post/Post.js
+++ src/Post/Post.js
@@ -3,82 +3,45 @@
    import Comment from '../Comment/Comment'
 
 export default class Post extends Component {
+   constructor(props){
+      super(props);
+      this.state = {
+         posts: []
+      }
+   }
+
+   componentDidMount() {
+      fetch('http://localhost:8000/content/posts', {
+         method: 'GET',
+         mode: 'cors',
+         credentials: 'same-origin',
+         headers: {
+            'Content-type': 'application/json'
+         }
+      })
+      .then(res => res.json())
+      .then(data => {
+         console.log(data)
+         this.setState({posts: data})
+      })
+   }
+
    render() {
       return(
          <ul>
-            <li className='post-box'>
-               <div id='post-title-post-1' className='post-header-box'>
-                  <h4 className='post-name'>Post 1</h4>
-                  <button className='comment-button' ><Link to='../../../Demo/Post-1/Comment/'>+ Comment</Link></button>
-                  <button className='update-button' ><Link to='../../../Demo/Post-1/Update/'>Update</Link></button>
-               </div>
-               <p>Post PostPost Post PostPostPostPost PostPost Post PostPostPost Post PostPost Post PostPostPost</p>
-               <Route path='/Demo/Post-1/Comment/' component={Comment} />
-            </li>
-            <li className='post-box'>
-               <div id='post-title-post-2' className='post-header-box'>
-                  <h4 className='post-name'>Post 2</h4>
-                  <button className='comment-button'><Link to='../../../Demo/Post-2/Comment/'>+ Comment</Link></button>
-                  <button className='update-button' ><Link to='../../../Demo/Post-2/Update/'>Update</Link></button>
-               </div>
-               <p>Post PostPost Post PostPostPostPost PostPost Post PostPostPost Post PostPost Post PostPostPost</p>
-               <Route path='/Demo/Post-2/Comment/' component={Comment} />
-            </li>
-
-            <li className='post-box'>
-                     <div id='post-title-post-3' className='post-header-box'>
-                        <h4 className='post-name'>Post 3</h4>
-                        <button className='comment-button'><Link to='../../../Demo/Post-3/Comment/'>+ Comment</Link></button>
-                  <button className='update-button' ><Link to='../../../Demo/Post-3/Update/'>Update</Link></button>
-                     </div>
-                     <p>Post PostPost Post PostPostPostPost PostPost Post PostPostPost Post PostPost Post PostPostPost</p>
-                     <Route path='/Demo/Post-3/Comment/' component={Comment} />
-                  </li>
-                  <li className='post-box'>
-                     <div id='post-title-post-4' className='post-header-box'>
-                        <h4 className='post-name'>Post 4</h4>
-                        <button className='comment-button'><Link to='../../../Demo/Post-4/Comment/'>+ Comment</Link></button>
-                  <button className='update-button' ><Link to='../../../Demo/Post-4/Update/'>Update</Link></button>
-                     </div>
-                     <p>Post PostPost Post PostPostPostPost PostPost Post PostPostPost Post PostPost Post PostPostPost</p>
-                     <Route path='/Demo/Post-4/Comment/' component={Comment} />
-                  </li>
-                  <li className='post-box'>
-                     <div id='post-title-post-5' className='post-header-box'>
-                        <h4 className='post-name'>Post 5</h4>
-                        <button className='comment-button'><Link to='../../../Demo/Post-5/Comment/'>+ Comment</Link></button>
-                  <button className='update-button' ><Link to='../../../Demo/Post-5/Update/'>Update</Link></button>
-                     </div>
-                     <p>Post PostPost Post PostPostPostPost PostPost Post PostPostPost Post PostPost Post PostPostPost</p>
-                     <Route path='/Demo/Post-5/Comment/' component={Comment} />
-                  </li>
-                  <li className='post-box'>
-                     <div id='post-title-post-6' className='post-header-box'>
-                        <h4 className='post-name'>Post 6</h4>
-                        <button className='comment-button'><Link to='../../../Demo/Post-6/Comment/'>+ Comment</Link></button>
-                  <button className='update-button' ><Link to='../../../Demo/Post-6/Update/'>Update</Link></button>
-                     </div>
-                     <p>Post PostPost Post PostPostPostPost PostPost Post PostPostPost Post PostPost Post PostPostPost</p>
-                     <Route path='/Demo/Post-6/Comment/' component={Comment} />
-                  </li>
-                  <li className='post-box'>
-                     <div id='post-title-post-7' className='post-header-box'>
-                        <h4 className='post-name'>Post 7</h4>
-                        <button className='comment-button'><Link to='../../../Demo/Post-7/Comment/'>+ Comment</Link></button>
-                  <button className='update-button' ><Link to='../../../Demo/Post-7/Update/'>Update</Link></button>
-                     </div>
-                     <p>Post PostPost Post PostPostPostPost PostPost Post PostPostPost Post PostPost Post PostPostPost</p>
-                     <Route path='/Demo/Post-7/Comment/' component={Comment} />
-                  </li>
-                  <li className='post-box'>
-                     <div id='post-title-post-8' className='post-header-box'>
-                        <h4 className='post-name'>Post 8</h4>
-                        <button className='comment-button'><Link to='../../../Demo/Post-8/Comment/'>+ Comment</Link></button>
-                  <button className='update-button' ><Link to='../../../Demo/Post-8/Update/'>Update</Link></button>
-                     </div>
-                     <p>Post PostPost Post PostPostPostPost PostPost Post PostPostPost Post PostPost Post PostPostPost</p>
-                     <Route path='/Demo/Post-8/Comment/' component={Comment} />
-                  </li>
+            {this.state.posts.map(post => {
+            return (
+               <li className='post-box'>
+                  <div id={post.postId}>
+                     <h4 className='post-name'>{post.title}</h4><h5>Posted by: {post.username} on: {post.post_date}</h5>
+                     <button className='comment-button' ><Link to={`../../../Demo/${post.postId}/Comment/`}>+ Comment</Link></button>
+                     <button className='update-button' ><Link to={`../../../Demo/${post.postId}/Update/`}>Update</Link></button>
+                  </div>
+                  <p>{post.content}</p>
+                  <Route path={`/Demo/${post.postId}/Comment/`} component={Comment} />
+               </li>
+            )
+            })}
          </ul>
    )}
 }
