b7c35238438ac024a68a9ff1d5d4a879c76e7a16
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-26 13:11:19 -0500

    fixed typo

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://pacific-mesa-99768.herokuapp.com/`,
+  API_ENDPOINT: `https://pacific-mesa-99768.herokuapp.com`,
   API_TOKEN: process.env.REACT_APP_API_TOKEN
 }
