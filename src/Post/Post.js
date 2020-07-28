254c2ee3be1a80a03b330bc2130dd72a24ba85a0
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-28 11:28:43 -0500

    update

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -98,7 +98,7 @@ class Post extends Component {
               />
               <Comments
                 postId={post.postId}
-                post_date={moment(post.post_date).fromNow()}
+                post_date={moment.utc(post.post_date).fromNow()}
                 username={post.username}
                 updateComments={this.updateComments}
               />
