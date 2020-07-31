60ec8ce998700bbd863b3f95252148bf8d8d4aeb
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-30 23:06:26 -0500

    update

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -39,7 +39,7 @@ class Post extends Component {
         return res.json();
       })
       .then(res => alert('Refresh the page to see your updated post.'))
-      .then(res =>this.props.history("/Demo"));
+      .then(this.props.history("/Demo"));
   };
 
   updateComments = () => {
