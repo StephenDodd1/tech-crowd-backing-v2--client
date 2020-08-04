3a8cac564d606405ec9869c85fb1c762262b9177
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-03 21:20:31 -0500

    update

--- src/Login/login-service.js
+++ src/Login/login-service.js
@@ -2,11 +2,11 @@ import config from "../config";
 
 const LoginService = {
   makeBasicAuthToken(username, password) {
-    console.log(username, ':', password)
     return window.btoa(`${username}:${password}`);
   },
   saveAuthToken(token) {
-    window.localStorage.setItem(config.TOKEN_KEY, token)
+    console.log(token)
+    return window.localStorage.setItem(config.TOKEN_KEY, token)
   },
   getAuthToken() {
     return window.localStorage.getItem(config.TOKEN_KEY)
