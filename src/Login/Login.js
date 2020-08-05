fad7ff433cf9e3a391808f4647cf1788b9364365
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-05 15:47:06 -0500

    update

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -15,7 +15,7 @@ export default class Login extends Component {
     super(props);
     this.state = {
       error: null,
-      userId: 1,
+      userId: 0,
     };
   }
   handleSubmitBasicAuth = (e) => {
@@ -38,6 +38,7 @@ export default class Login extends Component {
     })
       .then((res) => res.json())
       .then((data) => {
+         console.log(data)
         window.localStorage.setItem(config.JWT_TOKEN, data.jwtToken);
         this.setState({userId: data.userid})
         return this.onLoginSuccess();
