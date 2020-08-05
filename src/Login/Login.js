630a5cb50a2d4e2b5822746cd01e9eeb33610095
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-04 21:58:41 -0500

    update

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,67 +1,76 @@
-import React, { Component } from 'react';
-import config from '../config'
-import LoginService from './login-service';
+import React, { Component } from "react";
+import config from "../config";
+import LoginService from "./login-service";
 
 export default class Login extends Component {
-   static defaultProps = {
-      location: {},
-      history: {
-         push: () => {},
-      },
-      onLoginSuccess: () => {},
-   }
-   state = { 
+  static defaultProps = {
+    location: {},
+    history: {
+      push: () => {},
+    },
+    onLoginSuccess: () => {},
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
       error: null,
-      screenName: 'defaultUser',
+      screenName: "defaultUser",
     };
-   handleSubmitBasicAuth = (e) => {
-      e.preventDefault();
-      const { username, password } = e.target;
-      LoginService.saveAuthToken(
-         LoginService.makeBasicAuthToken(username.value, password.value)
-      );
-      console.log(LoginService.getAuthToken())
-      fetch(`${config.API_ENDPOINT}/api/user`, {
-         method: "POST",
-         mode: "cors",
-         credentials: "same-origin",
-         headers: {
-           "Content-type": "application/json",
-           Authorization: `basic ${LoginService.getAuthToken()}, Bearer ${config.API_TOKEN}`,
-         }
-      })
+  }
+  handleSubmitBasicAuth = (e) => {
+    e.preventDefault();
+    const { username, password } = e.target;
+    LoginService.saveAuthToken(
+      LoginService.makeBasicAuthToken(username.value, password.value)
+    );
+    console.log(LoginService.getAuthToken());
+    fetch(`${config.API_ENDPOINT}/api/user`, {
+      method: "POST",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `basic ${LoginService.getAuthToken()}, Bearer ${
+          config.API_TOKEN
+        }`,
+      },
+    })
       .then((res) => res.json())
-      .then(data => {
-         window.localStorage.setItem(config.JWT_TOKEN, data.jwtToken)
-         this.setState({screenName: username.value})
-         return this.onLoginSuccess();
-      })
-      username.value = '';
-      password.value = '';
-   }
-   onLoginSuccess = () => {
-
-      console.log(this.state.screenName)
-      const { location, history } = this.props;
-      const destination = (location.state || {}).from || '/Demo'
-      history.push(destination)
-   }
-   render() {
-      return(
-         <div id='login-container'>
-            <h3>LOGIN -- TechCrowdBacking</h3>
-            <form onSubmit={this.handleSubmitBasicAuth}>
-               <div className='input-label'>
-                  <label htmlFor='username'>Username</label>
-                  <input id='username' name='username' className='input'/>
-               </div>
-               <div className='input-label'>
-                  <label htmlFor='current-password'>Password</label>
-                  <input id='current-password' name='password' type='password' className='input'/>
-               </div>
-               <button type='submit'>Submit</button>
-            </form>
-         </div>
-      )
-   }
+      .then((data) => {
+        window.localStorage.setItem(config.JWT_TOKEN, data.jwtToken);
+        this.setState({ screenName: username.value });
+        return this.onLoginSuccess();
+      });
+    username.value = "";
+    password.value = "";
+  };
+  onLoginSuccess = () => {
+    console.log(this.state.screenName);
+    const { location, history } = this.props;
+    const destination = (location.state || {}).from || "/Demo";
+    history.push(destination);
+  };
+  render() {
+    return (
+      <div id="login-container">
+        <h3>LOGIN -- TechCrowdBacking</h3>
+        <form onSubmit={this.handleSubmitBasicAuth}>
+          <div className="input-label">
+            <label htmlFor="username">Username</label>
+            <input id="username" name="username" className="input" />
+          </div>
+          <div className="input-label">
+            <label htmlFor="current-password">Password</label>
+            <input
+              id="current-password"
+              name="password"
+              type="password"
+              className="input"
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
