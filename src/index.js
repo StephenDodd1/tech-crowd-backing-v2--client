856b6b327aa60179828b128d2d73fbffd5ad85ec
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-05 19:46:05 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-import { UserContext } from './Context';
+import { BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
- 
-    <BrowserRouter>
-      <UserContext>
-        <App />
-      </UserContext>
-    </BrowserRouter>,
- 
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
