e011c967de66a302ce14faa54204bd1d8b5a4fb5
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-08 08:47:39 -0500

    updating logout button

--- src/Logout/Logout.js
+++ src/Logout/Logout.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default function LogoutButton() {
   return (
     <div>
-      <button onclick={this.props.logout}>Logout</button>
+      <button onClick={props.logout}>Logout</button>
     </div>)
 }
