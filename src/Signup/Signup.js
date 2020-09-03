689daa09fe3b76a5718996cc511e19751e486a9f
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-09-03 08:54:41 -0500

    update routes

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import config from "../config";
 
 export default class Signup extends Component {
+  constructor(props){
+    super(props);
+  }
   handleSignup = (e) => {
     e.preventDefault();
     const t = e.target;
