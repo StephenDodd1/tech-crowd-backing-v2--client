bae86de1d79f8ef1d9ba5226977b77aaebb54b35
StephenDodd1 <skdodd@live.com>
2021-03-14 21:53:40 -0500

    finished editing, deleting logs
    debugging
    
    debugging
    
    debugging
    
    expected fix
    
    see what user object returns in context
    
    see comments object
    
    fix username

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -15,8 +15,14 @@ export default class Comments extends Component {
   handleDelete = (e) => {
     e.preventDefault();
     const userId = this.context.user.userId
+<<<<<<< HEAD
     if(userId !== Number(e.target.name)){
       alert(`You cannot delete comments you did not create.`)
+=======
+    if(userId !== e.target.name){
+      console.log(this.context.user,e)
+      alert(`${userId} you cannot delete posts you did not create. Do you need to sign into the account for ${e.target.name}?`)
+>>>>>>> ff59363... expected fix
       return;
     }
     fetch(`${config.API_ENDPOINT}/api/comments/${e.target.value}`, {
