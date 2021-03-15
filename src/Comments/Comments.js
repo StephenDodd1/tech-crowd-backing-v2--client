67c9f05c5dafc0ee4d3cf95914b6c32dacefb62c
StephenDodd1 <skdodd@live.com>
2021-03-14 19:38:32 -0500

    debugging

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -14,7 +14,7 @@ export default class Comments extends Component {
 
   handleDelete = (e, uid) => {
     e.preventDefault();
-    if(uid !== e.name.userId){
+    if(uid !== e.target.name){
       console.log(uid,e)
       alert(`${uid} you cannot delete posts you did not create. Do you need to sign into the account for ${e.name.userId}?`)
       return;
