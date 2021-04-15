eeb5a99e7dbf2ce9bb9153e1f1f944a91401be59
StephenDodd1 <skdodd@live.com>
2021-04-14 22:31:14 -0500

    update API token

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   API_ENDPOINT: process.env.API_ENPOINT || "https://pacific-mesa-99768.herokuapp.com",
-  API_TOKEN: process.env.REACT_APP_API_TOKEN,
+  API_TOKEN: process.env.API_TOKEN,
   TOKEN_KEY: 'client-auth-token',
   JWT_TOKEN: 'jwt-token'
 }
